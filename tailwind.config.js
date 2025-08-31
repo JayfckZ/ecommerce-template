e640eb50367178825c558215bea7e19dab56ec60
JayfckZ <joaomarcelo1015ja@gmail.com>
2025-08-30 22:52:23 -0300

    feat: esquema de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: [".index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coffee: {
+          light: "#F5EBDD",   // fundo
+          DEFAULT: "#D6BFA7", // caixas/divs
+          dark: "#8B5E3C",    // botões
+          rich: "#4B2E1E",    // texto
+        }
+      }
+    },
   },
   plugins: [],
 }
